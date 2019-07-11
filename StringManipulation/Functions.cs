1465bcb3dd57c1ba7f2106966162a8f6ab451842
jaishreeK <jaishreekulkarni@gmail.com>
2019-07-11 21:42:00 +1200

    added new function Codility demo test

---
+++ StringManipulation/Functions.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    public class Functions
+    {
+        // Complete the alternatingCharacters function below.
+        public int alternatingCharacters(string s)
+        {
+            int numDeletes = 0;           
+            int index = 0;
+            int diff, nextIndex;
+            while (index < s.Length)
+            {                
+                if (s.ElementAt(index) == 'A')                                    
+                     nextIndex = s.IndexOf('B',index);
+                else
+                     nextIndex = s.IndexOf('A',index);
+                
+                if (nextIndex > 0)
+                    diff = nextIndex - index;
+                else
+                    diff = s.Length - index;
+
+                if (diff > 1)
+                {
+                    s = s.Remove(index + 1, diff - 1);
+                    numDeletes = numDeletes + diff - 1;
+                }       
+                
+                index++;
+            }           
+            return numDeletes;
+
+        }
+
+        public int alternatingCharacters1(string s)
+        {
+            int count = 0;
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (s[i] == s[i + 1])
+                    count++;
+            }
+
+            return count;
+        }
+
+        // Complete the makeAnagram function below.
+        public int makeAnagram(string a, string b)
+        {
+            int numDeletes = 0;
+
+            char[] aCharArray = a.ToLower().ToCharArray();
+            char[] bCharArray = b.ToLower().ToCharArray();
+                      
+            char[] aDiff = aCharArray.Except(bCharArray).ToArray();
+            char[] bDiff = bCharArray.Except(aCharArray).ToArray();
+            int[] charCountsA = new int[aDiff.Length];
+            for(int i=0;i<aDiff.Length;i++)
+            {
+                //find out no. of repetitions of each character in the first string
+               charCountsA[i] = a.Count(x => x == aDiff[i]);
+
+            }
+            int sumADiff = charCountsA.Sum();
+            int[] charCountsB= new int[bDiff.Length];
+            for (int i = 0; i < bDiff.Length; i++)
+            {
+                //find out no. of repetitions of each character in the first string
+                charCountsB[i] = b.Count(x => x == bDiff[i]);
+
+            }
+
+            int sumBDiff = charCountsB.Sum();
+
+            numDeletes = sumADiff + sumBDiff;
+           
+            return numDeletes;
+        }
+
+        public int solution(int[] A)
+        {
+            int num = 1;           
+            List<int> listA = new List<int>(A);
+            listA.RemoveAll(x => x < 0); 
+            while (listA.Count > 0)
+            {
+                if (listA.Find(x => x == num) > 0)
+                {
+                    listA.RemoveAll(x => x == num);
+                    num++;
+                }
+                else break;
+            }
+            return num;
+        }
+
+    }
+}
---
+++ StringManipulationTests/FunctionTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using StringManipulation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace StringManipulationTests
+{
+    [TestClass]
+    public class FunctionTests
+    {
+        Functions func = new Functions();
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //    //            5
+        //    //AAAA
+        //    //BBBBB
+        //    //ABABABAB
+        //    //BABABA
+        //    //AAABBB
+        //    //func.alternatingCharacters("AAAA");
+        //    func.alternatingCharacters("AAABBB");
+        //}
+
+        [TestMethod()]
+        public void solutionTest()
+        {
+            int[] A = new int[] { 1, 3, 6, 4, 1, 2 };
+            int r =  func.solution(A);
+            Assert.AreEqual(5,r);
+        }
+
+        //[TestMethod()]
+        //public void makeAnagramTest()
+        //{
+        //    string a = "fcrxzwscanmligyxyvym";
+        //    string b = "jxwtrhvujlmrpdoqbisbwhmgpmeoke";
+        //   func.makeAnagram(a, b);
+        //    Assert.Fail();
+        //}
+        
+    }
+}
